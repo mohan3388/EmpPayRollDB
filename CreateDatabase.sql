986552f6c748631f8c7d5eb0c06c9d407c97cc12
mohan338 <sahumohan338@gmail.com>
2022-06-11 16:30:53 +0530

    [mohan]add: select Salary particular Employee

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,4 +1,23 @@
 create database EmpPayRoll_Services
 
-use Emp_Services
+use EmpPayRoll_Services
 
+create table EmpPayRoll
+(
+ID int IDENTITY(1,1) primary key,
+Name varchar (20),
+Salary int,
+SatrtDate Date 
+);
+
+insert into EmpPayRoll values('mohan',23000,'2019.APR.22')
+insert into EmpPayRoll values('govind',20000,'2022.JUN.01')
+insert into EmpPayRoll values('bhuppi',16500,'2013.MAY.15')
+insert into EmpPayRoll values('rajesh',17000,'2014.FEB.22')
+
+select * from EmpPayRoll
+
+
+select Salary from EmpPayRoll where Name='mohan';
+
+select SatrtDate from EmpPayRoll WHERE (SatrtDate BETWEEN '2019-04-22' AND GETDATE());
