3612c8bd5743eb978a8bef103d16606ffb99964e
mohan3388 <68227271+mohan3388@users.noreply.github.com>
2022-06-11 16:07:30 +0530

    Update CreateDatabase.sql

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -2,3 +2,17 @@ create database EmpPayRoll_Services
 
 use Emp_Services
 
+create table EmpPayRoll
+(
+ID int IDENTITY(1,1) primary key,
+Name varchar (20),
+Salary int,
+SatrtDate Date 
+);
+
+insert into EmpPayRoll values('mohan',23000,'2019.APR.22')
+insert into EmpPayRoll values('govind',20000,'2022.JUN.01')
+insert into EmpPayRoll values('bhuppi',16500,'2013.MAY.15')
+insert into EmpPayRoll values('rajesh',17000,'2014.FEB.22')
+
+select * from EmpPayRoll
