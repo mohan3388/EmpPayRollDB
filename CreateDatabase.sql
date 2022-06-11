08c4dbd8a7010bd043696de92aec4435c9508e16
mohan338 <sahumohan338@gmail.com>
2022-06-11 17:34:43 +0530

    [mohan]add: find sum avg min max and number of male and female employee

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -10,10 +10,11 @@ Salary int,
 SatrtDate Date 
 );
 
-insert into EmpPayRoll values('mohan',23000,'2019.APR.22')
+insert into EmpPayRoll values('pooja',23000,'2019.APR.22')
 insert into EmpPayRoll values('govind',20000,'2022.JUN.01')
 insert into EmpPayRoll values('bhuppi',16500,'2013.MAY.15')
 insert into EmpPayRoll values('rajesh',17000,'2014.FEB.22')
+
 
 select * from EmpPayRoll
 
@@ -23,3 +24,21 @@ select Salary from EmpPayRoll where Name='mohan';
 select SatrtDate from EmpPayRoll WHERE (SatrtDate BETWEEN '2019-04-22' AND GETDATE());
 
  Alter table EmpPayRoll Add Gender varchar(1);
+
+ Update EmpPayRoll set Gender='M' where Id in(1,3);
+
+ Update EmpPayRoll set Gender='F' where Id in(2,4);
+
+ select Name from EmpPayRoll where Gender= 'F';
+
+ select SUM(Salary) from EmpPayRoll;
+
+ select SUM(Salary) from EmpPayRoll where Gender='F' group by Gender;
+
+ select AVG(Salary) from EmpPayRoll where Gender='M' group by Gender;
+
+ select MIN(Salary) from EmpPayRoll;
+
+  select MAX(Salary) from EmpPayRoll;
+ 
+ select COUNT(ID) from EmpPayRoll;
